42f3faebb75dcc375627ff095ff3a61189565cac
Joshua Eng <engch28@gmail.com>
2021-11-24 14:46:16 +0800

    Yelp MVP Completed

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,30 @@
+class ReviewsController < ApplicationController
+
+  def show
+    @review = Review.new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  def new
+    @review = Review.new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :content)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning DB 🧹"
+Restaurant.destroy_all
+Review.destroy_all
+
+puts "Generating Seed 🌱"
+
+50.times do |index|
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::Number.number(digits: 8),
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+
+  Review.create(
+    content: Faker::Restaurant.review,
+    rating: rand(0..5),
+    restaurant_id: index + 1
+  )
+end
+
+puts "Completed"
